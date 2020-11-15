60c172c056471b1b40af32f8d6b4abe8773e8c65
SUITE <SUITE@SUITEA201-ST19.cibertec.edu.pe>
2020-11-15 15:12:42 -0500

    prueba 2

--- DSW_Cibertec/src/main/java/com/dsw/repository/AlumnoRepositorio.java
+++ DSW_Cibertec/src/main/java/com/dsw/repository/AlumnoRepositorio.java
@@ -2,8 +2,11 @@ package com.dsw.repository;
 
 import java.util.List;
 
+import javax.persistence.QueryHint;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 
 import com.dsw.entidad.Alumno;
@@ -29,6 +32,7 @@ public interface AlumnoRepositorio extends JpaRepository<Alumno, Integer>{
 	@Query("Select a from Alumno a where a.usuario.idusuario = :idusu")
 	public abstract Alumno getAlumno(@Param("idusu")Integer idusuario);
 	
-	@Query("Select distinct a from Alumno a,Alumno_Clase ac,Clase c where a.idalumno = ac.alumno.idalumno and ac.clase.idclase = c.idclase and c.docente.iddocente = :iddoc")
+	@Query("Select DISTINCT a from Alumno a,Alumno_Clase ac,Clase c where a.idalumno = ac.alumno.idalumno and ac.clase.idclase = c.idclase and c.docente.iddocente = :iddoc")
+	@QueryHints(value = {@QueryHint(name = org.hibernate.jpa.QueryHints.HINT_PASS_DISTINCT_THROUGH,value = "false")})
 	public abstract List<Alumno> getAlumnosXDocente(@Param("iddoc")Integer iddocente);
 }
