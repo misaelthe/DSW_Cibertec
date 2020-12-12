385666804a1c48e02dfc3422e638515c52672e80
SUITE <SUITE@SUITEA201-ST19.cibertec.edu.pe>
2020-12-12 10:52:20 -0500

    1.4.31

--- DSW_Cibertec/src/main/java/com/dsw/controller/DocenteController.java
+++ DSW_Cibertec/src/main/java/com/dsw/controller/DocenteController.java
@@ -34,59 +34,73 @@ public class DocenteController {
 	private DocenteServicio ser_docente;
 
 	@RequestMapping("/verIndexDocente")
-	public String verIndexDocente() {return "docente/indexDocente";}
+	public String verIndexDocente() {
+		return "docente/indexDocente";
+	}
+
 	@RequestMapping("/verPageListarClase")
-	public String verPageListarClase() {return "docente/pageListarClase";}
+	public String verPageListarClase() {
+		return "docente/pageListarClase";
+	}
+
 	@RequestMapping("/verPageListarAlumnos")
-	public String verPageListarAlumnos(String idclase,HttpSession session) {
-		session.setAttribute("claseSeleccionada",idclase);
+	public String verPageListarAlumnos(String idclase, HttpSession session) {
+		session.setAttribute("claseSeleccionada", idclase);
 		return "docente/pageListarAlumnos";
 	}
+
 	@RequestMapping("/verPageRegistrarNotas")
-	public String verPageRegistrarNotas(String idalumno,HttpSession session) {
-		session.setAttribute("alumnoSeleccionado",idalumno);
+	public String verPageRegistrarNotas(String idalumno, HttpSession session) {
+		session.setAttribute("alumnoSeleccionado", idalumno);
 		return "docente/pageRegistrarNotas";
 	}
+
 	@RequestMapping("/registrarNotas")
-	public String registrarNotas(Integer inTeo1,Integer inTeo2,Integer inPar,Integer inTeo3,Integer inFin,HttpSession session) {
-		Integer idclase=Integer.parseInt((String)session.getAttribute("claseSeleccionada"));
-		Integer idalumno=Integer.parseInt((String)session.getAttribute("alumnoSeleccionado"));
-		Nota nota=ser_alumno.getNotaXAlumnoXClase(idalumno, idclase);
+	public String registrarNotas(Integer inTeo1, Integer inTeo2, Integer inPar, Integer inTeo3, Integer inFin,
+			HttpSession session) {
+		Integer idclase = Integer.parseInt((String) session.getAttribute("claseSeleccionada"));
+		Integer idalumno = Integer.parseInt((String) session.getAttribute("alumnoSeleccionado"));
+		Nota nota = ser_alumno.getNotaXAlumnoXClase(idalumno, idclase);
 		nota.setE1(inTeo1);
 		nota.setE2(inTeo2);
 		nota.setEp(inPar);
 		nota.setE3(inTeo3);
 		nota.setEf(inFin);
-		Integer no1= Math.round((inTeo1+inTeo2+inTeo3)*45/300);
-		Integer no2=Math.round((inPar)*20/100);
-		Integer no3=Math.round((inFin)*25/100);
-		Integer prom=no1+no2+no3;
-		System.out.println(prom.toString()+"-------------------------------------------------------------------------------"+no1+"-"+no2+"-"+no3);
+		Integer no1 = (int) Math.ceil((inTeo1 + inTeo2 + inTeo3) * 45 / 300);
+		Integer no2 = (int) Math.ceil((inPar) * 20 / 100);
+		Integer no3 = (int) Math.ceil((inFin) * 25 / 100);
+		Integer prom = no1 + no2 + no3;
+		System.out.println(
+				prom.toString() + "-------------------------------------------------------------------------------"
+						+ no1 + "-" + no2 + "-" + no3);
 		nota.setPromedio(prom);
 		ser_alumno.insertNota(nota);
 		return "docente/indexDocente";
 	}
-	@RequestMapping(value="/getClasesXDocente",method = RequestMethod.GET,produces = "application/json")
+
+	@RequestMapping(value = "/getClasesXDocente", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
 	public ResponseEntity<List<Clase>> getClasesXDocente(HttpSession session) {
-		Usuario u=(Usuario)session.getAttribute("objUsuario");
-		Docente d=ser_docente.getDocenteXIdusuario(u.getIdusuario());
-		List<Clase> tem=ser_docente.getClaseXDocente(d.getIddocente());
+		Usuario u = (Usuario) session.getAttribute("objUsuario");
+		Docente d = ser_docente.getDocenteXIdusuario(u.getIdusuario());
+		List<Clase> tem = ser_docente.getClaseXDocente(d.getIddocente());
 		return new ResponseEntity<>(tem, HttpStatus.OK);
 	}
-	@RequestMapping(value="/getAlumnosXClase",method = RequestMethod.GET,produces = "application/json")
+
+	@RequestMapping(value = "/getAlumnosXClase", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
 	public ResponseEntity<List<Alumno>> getAlumnosDocenteCurso(HttpSession session) {
-		Integer idclase=Integer.parseInt((String)session.getAttribute("claseSeleccionada"));
-		List<Alumno> tem=ser_alumno.getAlumnosXClase(idclase);
+		Integer idclase = Integer.parseInt((String) session.getAttribute("claseSeleccionada"));
+		List<Alumno> tem = ser_alumno.getAlumnosXClase(idclase);
 		return new ResponseEntity<>(tem, HttpStatus.OK);
 	}
-	@RequestMapping(value="/getNotaXAlumnoXClase",method = RequestMethod.GET,produces = "application/json")
+
+	@RequestMapping(value = "/getNotaXAlumnoXClase", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
 	public ResponseEntity<Nota> getNotaXAlumnoXClase(HttpSession session) {
-		Integer idalumno=Integer.parseInt((String)session.getAttribute("alumnoSeleccionado"));
-		Integer idclase=Integer.parseInt((String)session.getAttribute("claseSeleccionada"));
-		Nota tem=ser_alumno.getNotaXAlumnoXClase(idalumno, idclase);
+		Integer idalumno = Integer.parseInt((String) session.getAttribute("alumnoSeleccionado"));
+		Integer idclase = Integer.parseInt((String) session.getAttribute("claseSeleccionada"));
+		Nota tem = ser_alumno.getNotaXAlumnoXClase(idalumno, idclase);
 		return new ResponseEntity<Nota>(tem, HttpStatus.OK);
 	}
 }
